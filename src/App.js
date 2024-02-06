46e3418a0fea02c585090d082d4f2f107f47f4e5
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-06 19:30:48 +0530

    assets Added & Folder Structure Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
 import './App.css';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Experience from './Components/Experience/Experience';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Navbar from './Components/Navbar/Navbar';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Services from './Components/Services/Services';
+import Testimonial from './Components/Testimonial/Testimonial';
 
 function App() {
   return (
     <div className="App">
-     
+    <Header/>
+    <Navbar/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+
+
+   
+    
     </div>
   );
 }
