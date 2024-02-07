af8d411de40718bc832c1187f7aa6eef3f392698
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-07 23:09:55 +0530

    minor mistakes rectified

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,12 +10,14 @@ const Header = () => {
       <div className="container header__container">
         <h5> Hey I'm </h5>
         <h1> Akash S M </h1>
-        <h5 className="text-light"> Full Stack Develoepr  </h5>
+        <h5 className="text-light"> Full Stack Develoepr </h5>
+        <div>  
         <CTA />
         <Socials />
         <div className="me">
-          <img src={Image1} alt="mypic"  />
-        </div>
+        <img src={Image1} alt="mypic" />
+      </div>
+     </div>
         <a href="#Contact" className="scroll__down">
           Scroll Down
         </a>
