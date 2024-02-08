17894c50615b4d9396283354d9077a51a62778e8
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-08 20:04:30 +0530

    changes needed in Portfolio section

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import "./Portfolio.css"
+import React from "react";
+import "./Portfolio.css";
+import Qtrip from "../../Assets/Qtrip-static.png";
+import Xboard from "../../Assets/Xboard.png";
+import Qkart from "../../Assets/Qkart.png";
+import Qtify from "../../Assets/qtify.png";
 
 const Portfolio = () => {
   return (
-   <section id='Portfolio'> Portfolio </section>
-  )
-}
+    <section id="Portfolio">
+      <h5> My Recent Works </h5>
+      <h2> Portfolio </h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+          <img src={Qtrip} alt="qtrip" />
+           
+          </div>
+          <h3> This is the Title </h3>
+          <a href="https://github.com" className="btn">Github</a>
+          <a
+            href="https://local.host.com"
+            className="btn btn-primary"
+            target="blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
