bef74cfccb9f30144894ddc64efa5ea825a72df0
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-10 11:11:35 +0530

    changes needed in contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import "./Contact.css"
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+
 
 const Contact = () => {
   return (
-   <section id='Contact'>  Contact </section>
+   <section id='Contact'>  
+   
+   <h5> Get in Touch </h5>
+   <h2> Contact Me </h2>
+
+
+   <div className="container contact__container">
+   <div className="contact__options">
+
+ <article className="contact__options">
+ <MdOutlineMarkEmailUnread/>
+ <h4> Email  </h4> 
+ <h5> akashvicky2012@gmail.com</h5>
+ <a href="mailto: akashvicky2012@gmail.com"> Send a  Mail</a>
+ 
+ </article>
+ <article className="contact__options">
+ <MdOutlineMarkEmailUnread/>
+ <h4> Whatsapp  </h4> 
+ <h5> +91 9150253488 </h5>
+ <a href="https://api.whatsapp.com/send?phone=+919150253488">  Whatsapp Me </a>
+ 
+ </article>
+ <article className="contact__options">
+ <MdOutlineMarkEmailUnread/>
+ <h4> Email  </h4> 
+ <h5> akashvicky2012@gmail.com</h5>
+ <a href="mailto: akashvicky2012@gmail.com"> Send a  Mail</a>
+ 
+ </article>
+   
+   
+   </div>
+
+   <form action=''> </form>
+   
+   </div>
+   </section>
   )
 }
 
