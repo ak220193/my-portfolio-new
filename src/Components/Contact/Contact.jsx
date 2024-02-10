2f99ed750938b0bc5b2dcaba8c0036db62a8d2a5
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-10 18:30:19 +0530

    media queries are pending for Contact Section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Contact.css"
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
+import { ImWhatsapp } from "react-icons/im";
 
 
 const Contact = () => {
@@ -14,32 +16,36 @@ const Contact = () => {
    <div className="container contact__container">
    <div className="contact__options">
 
- <article className="contact__options">
- <MdOutlineMarkEmailUnread/>
+ <article className="contact__option">
+ <MdOutlineMarkEmailUnread className='contact__option-icon'/>
  <h4> Email  </h4> 
  <h5> akashvicky2012@gmail.com</h5>
- <a href="mailto: akashvicky2012@gmail.com"> Send a  Mail</a>
+ <a href="https://gmail.com" target='blank'> Send a  Mail</a>
  
  </article>
- <article className="contact__options">
- <MdOutlineMarkEmailUnread/>
+ <article className="contact__option">
+ <ImWhatsapp className='contact__option-icon'/>
  <h4> Whatsapp  </h4> 
  <h5> +91 9150253488 </h5>
- <a href="https://api.whatsapp.com/send?phone=+919150253488">  Whatsapp Me </a>
+ <a href="https://wa.me/+919150253488" target='blank'>  Whatsapp Me </a>
  
  </article>
- <article className="contact__options">
- <MdOutlineMarkEmailUnread/>
- <h4> Email  </h4> 
- <h5> akashvicky2012@gmail.com</h5>
- <a href="mailto: akashvicky2012@gmail.com"> Send a  Mail</a>
- 
+ <article className="contact__option">
+ <FaLinkedin className='contact__option-icon'/>
+ <h4> Linkedin  </h4> 
+ <h5> Akash S M </h5>
+ <a href="https://www.linkedin.com/in/akash-s-m/" target='blank'> Connect With Linkedin</a>
  </article>
-   
    
    </div>
 
-   <form action=''> </form>
+   <form action="">
+   
+    <input type='text' name='name' placeholder='Your Full Name' required />
+    <input type='email' name='email' placeholder='Your E-mail Address'  required/>
+    <textarea name="message"  rows="7" placeholder='your Message' />
+    <button type='submit'  className='btn btn-primary'> Send a Message </button>
+   </form>
    
    </div>
    </section>
